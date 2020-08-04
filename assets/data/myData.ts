f4a90c6b4529bf1aa6600b76caadba4c32c3cc5e
khan454 <50718502+khan454@users.noreply.github.com>
2020-08-04 16:20:32 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,9 @@
+import { Khan454 } from "../../app/khan454";
+
+export const MyFilledData: Khan454 = {
+    name: "Jawad Khan",
+    loginName: "khan454",
+    studentNum: 991556484,
+    campus: "Davis",
+    assignmentTitle: "Assignment 4"
+};
